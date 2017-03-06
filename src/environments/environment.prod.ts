88c7e6a8d255f2c4caaa3a551566a3a04ccf328b
Maximilian Kramp <maxi.kramp@gmail.com>
2017-03-06 19:08:13 +0100

    environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
-  APP_ID: "17686e49eed56c343fe6106c44a232ed"
+  APP_ID: "17686e49eed56c343fe6106c44a232ed",
+  backendURL: "http://sails.arcticon.de/",
+  backendURLApartment: "apartment",
+  backendURLLogin: "login",
+  backendURLLogout: "logout",
+  backendURLUser: "user",
 };
